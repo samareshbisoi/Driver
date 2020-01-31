6e990a074bad7626b1ea9239e422f52922ebef6b
Subhadarsini Sahoo <subhadarsinisahoo@gmail.com>
2020-01-31 14:57:54 +0530

    first commit

---
+++ datajs/leftmenu.js
@@ -0,0 +1,26 @@
+function show_tab(tone,ttwo,bone,btwo) {
+	//alert(tone);
+	$(ttwo).hide();	
+	$(tone).show();	
+	
+	//alert();
+	$(btwo).removeClass('active');
+	//$(tone).removeClass('active');
+	$(bone).addClass('active');
+}
+
+// <li><a href="faculty-registermynewmobile.html" class="close-panel"><img src="images/icons/red/mobile.png" alt="" title="" /><span>Register My New Mobile</span></a></li>
+
+$(document).ready(function()
+		{
+		// Using GetJSON
+		//var url="http://localhost/phonegap/database/json.php";
+		var menu = '<li><a href="index.html" class="close-panel"><img src="images/icons/red/home.png" alt="" title="" /><span>Home</span></a></li><li><a href="english-home.html" class="close-panel"><img src="images/icons/red/home.png" alt="" title="" /><span>English</span></a></li><li><a href="hindi-home.html" class="close-panel"><img src="images/icons/red/home.png" alt="" title="" /><span>Hindi</span></a></li>';
+		
+		$("#sidemenu").append(menu);
+		
+		/*var RIGHTmenu = '<li><a href="#" class="close-panel"><img src="images/icons/red/settings.png" alt="" title="" /><span>Account Settings</span></a></li><li><a href="#" class="close-panel"><img src="images/icons/red/briefcase.png" alt="" title="" /><span>My Account</span></a></li><li><a href="#" class="close-panel"><img src="images/icons/red/message.png" alt="" title="" /><span>Notice</span><strong>12</strong></a></li><li><a href="#" class="close-panel"><img src="images/icons/red/love.png" alt="" title="" /><span>Class</span><strong>5</strong></a></li><li><a href="logout.html" class="close-panel"><img src="images/icons/red/lock.png" alt="" title="" /><span>Logout</span></a></li>';
+		
+		$("#rightmenu").append(RIGHTmenu);*/
+		
+	});
